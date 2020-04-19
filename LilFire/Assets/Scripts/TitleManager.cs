0aa84ad15dc9d4644bfbad8a7e459c7a13c7dedb
Siyuan <finalspace.1031@gmail.com>
2020-04-19 15:50:33 -0700

    title screen

--- LilFire/Assets/Scripts/TitleManager.cs
+++ LilFire/Assets/Scripts/TitleManager.cs
@@ -1,7 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleManager : SingletonBehaviour<TitleManager>
 {
+    public string titleSceneName;
+    public void OnStartGameClicked()
+	{
+        MainGameManager.Instance.GameStart();
+        Exit();
+    }
+
+    public void OnCreditsClicked()
+	{
+
+	}
+
+    public void Exit()
+    {
+        SceneManager.UnloadSceneAsync(titleSceneName);
+    }
 }

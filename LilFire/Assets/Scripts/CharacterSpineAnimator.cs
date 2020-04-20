a456b7ee6d5abd5dc84cd8a57a5a6a69d33146ee
Siyuan <finalspace.1031@gmail.com>
2020-04-19 21:23:27 -0700

    character animation

--- LilFire/Assets/Scripts/CharacterSpineAnimator.cs
+++ LilFire/Assets/Scripts/CharacterSpineAnimator.cs
@@ -1,11 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine.Unity;
 
 public class CharacterSpineAnimator : MonoBehaviour
 {
+	public SkeletonAnimation skeletonAnimation;
+	public AnimationReferenceAsset idle, walking;
+	public string currentState;
+	public float speed;
+	public float movement;
+
+    void Start()
+	{
+		currentState = "Idle";
+        SetCharacterState(currentState);
+    }
+        
     public void PlayEat()
     {
 
     }
+
+    public void SetAnimatoin(AnimationReferenceAsset animation, bool loop, float timeScale)
+	{
+		skeletonAnimation.state.SetAnimation(0, animation, loop).TimeScale = timeScale;
+	}
+
+    public void SetCharacterState(string state)
+	{
+		if (state.Equals("Idle"))
+			SetAnimatoin(idle, true, 1f);
+	}
 }

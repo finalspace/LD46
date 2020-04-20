017418543916e09c145ea2e76f1c6a93f76d5a8b
Lyndon Goodacre <lfgoodacre@gmail.com>
2020-04-20 00:01:32 -0400

    cleaned up trial level C for editing

--- LilFire/Assets/Scripts/WaypointTrigger.cs
+++ LilFire/Assets/Scripts/WaypointTrigger.cs
@@ -8,12 +8,8 @@ public class WaypointTrigger : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("WAYPOINT REACHED");
-        Debug.Log("REACHED BEFORE?" + reached);
-        Debug.Log("OTHER TAG:" + other.tag);
         if (!reached && other.tag == "Player")
         {
-            Debug.Log("AND TRIGGERED");
             // Fire the method that extends the level upwards
             BoardManager lvl = GameObject.FindObjectOfType<LevelManager>().GetComponent<BoardManager>();
             lvl.WaypointReached();

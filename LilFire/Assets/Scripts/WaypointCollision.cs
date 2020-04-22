81c6d54e67b018022d1387b22b201f3be7fade54
Lyndon Goodacre <lfgoodacre@gmail.com>
2020-04-21 23:06:26 -0400

    waypoint collisions now detected, will add energy behavior for waypoints next

--- LilFire/Assets/Scripts/WaypointCollision.cs
+++ LilFire/Assets/Scripts/WaypointCollision.cs
@@ -13,15 +13,15 @@ public class WaypointCollision : MonoBehaviour
     //        PlayerStats.Instance.losingEnergy = true;
     //    }
     //}
-    void OnCollisionEnter(Collision other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Energy steady while at camp!");
         if (other.gameObject.tag == "Player")
         {
             // while player is here, energy doesn't run down
             // also, tentatively, we'll replenish it to 100%
-            PlayerStats.Instance.losingEnergy = false;
-            PlayerStats.Instance.energy = 100;
+            //PlayerStats.Instance.losingEnergy = false;
+            //PlayerStats.Instance.energy = 100;
         }
     }
 
@@ -30,7 +30,7 @@ public class WaypointCollision : MonoBehaviour
         Debug.Log("Losing energy again! 2D");
         if (other.gameObject.tag == "Player")
         {
-            PlayerStats.Instance.losingEnergy = true;
+            //PlayerStats.Instance.losingEnergy = true;
         }
     }
     //void OnCollisionEnter2D(Collision2D other)
